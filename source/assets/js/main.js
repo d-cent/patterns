e4a3b3a9cef4bd02d044199b8ba80b35da4be8bb
Sebastian Saariokari <sebastian.saariokari@frantic.com>
2014-10-30 11:14:20 +0100

    added helper text pattern

--- source/assets/js/main.js
+++ source/assets/js/main.js
@@ -1,8 +1,16 @@
 
 $(document).ready(function() {
 
+	// Sign in
+
 	$('.js-open-signin').on('click', function() {
 		$('.login').toggleClass('js-show');
+	});
+
+	// helper text
+
+	$('.js-helper__toggle').on('click', function() {
+		$('.helper__body').toggleClass('js-show');
 	});
 
 });
