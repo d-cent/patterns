6dbda45e65725b2d0bd44eddc3dbd6b9e42e22b1
Sebastian Saariokari <sebastian.saariokari@frantic.com>
2014-11-03 16:56:55 +0100

    added fader

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -4,50 +4,60 @@ $(document).ready(function() {
 	// Sign in
 
 	$('.js-open-signin').on('click', function() {
-		$('.login').toggleClass('js-show');
+		$('.login').slideToggle(300);
 	});
 
 	// helper text
 
 	$('.js-helper__toggle').on('click', function() {
-		$('.helper__body').toggleClass('js-show');
+		$('.helper__body').slideToggle(300);
 	});
 
 
+	if ($('.fadeout').length) {
 
-		var $el, $ps, $up, totalHeight;
+		var orig_text;
 
-		$(".fader .btn").click(function() {
-					
-			totalHeight = 0
+		$('.fadeout').each(function () {
+			// console.log( $(this).children(":first")[0].clientHeight );
+			if ($(this).children(":first")[0].clientHeight < 160) {
+				$(this).parent().addClass('open');
+				$(this).addClass('pt-fix');
+				$(this).parent().find('.read-more').remove();
+			}
+		});
 
-			$el = $(this);
-			$p  = $el.parent();
-			$up = $p.parent();
-			$ps = $up.find("p:not('.read-more')");
-			
-			// measure how tall inside should be by adding together heights of all inside paragraphs (except read-more paragraph)
-			$ps.each(function() {
-				totalHeight += $(this).outerHeight();
-			});
-						
-			$up
-				.css({
-					// Set height to prevent instant jumpdown when max height is removed
-					"height": $up.height(),
-					"max-height": 9999
-				})
-				.animate({
-					"height": totalHeight
-				});
-			
-			// fade out read-more
-			$p.fadeOut();
-			
-			// prevent jump-down
-			return false;
+		//$('.fadeout.clickable').click(function (event) {
+		//    event.preventDefault();
 
-			
-});
+		//    $(this).addClass('open');
+		//    $(this).removeClass('clickable');
+		//    $(this).find('a').addClass('pt-fix');
+		//    $(this).find('.read-more').remove();
+		//});
+
+		$('.read-more').find('a').click(function (event) {
+			event.preventDefault();
+
+			orig_text = $(this).text();
+			$(this).parent().parent('.fadeout').addClass('open');
+			$(this).addClass('pt-fix');
+			$(this).parent().remove();
+
+			// In case toggle between 'lue lisää' and 'pienennä teksti' is wanted
+
+			//$('.fadeout').toggleClass('open');
+			/*
+			if( $('.fadeout').hasClass('open') ){
+				$(this).text('Pienennä teksti');
+				$(this).addClass('pt-fix');
+			}else{
+				$(this).text(orig_text);
+				$(this).removeClass('pt-fix');
+			}*/
+		});
+	}
+
+		
 
 });
