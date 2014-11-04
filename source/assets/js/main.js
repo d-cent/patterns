6104db623f713a6c0a1404a8ea7085b817c38d03
Sebastian Saariokari <sebastian.saariokari@frantic.com>
2014-11-04 14:35:52 +0100

    added objects to patterns

--- source/assets/js/main.js
+++ source/assets/js/main.js
@@ -1,6 +1,20 @@
 
 $(document).ready(function() {
 
+	// svg fallback
+
+	if (!Modernizr.svg) {
+		var imgs = document.getElementsByTagName('img');
+		var endsWithDotSvg = /.*\.svg$/;
+		var i = 0;
+		var l = imgs.length;
+		for (; i != l; ++i) {
+			if (imgs[i].src.match(endsWithDotSvg)) {
+				imgs[i].src = imgs[i].src.slice(0, -3) + 'png';
+			}
+		}
+	}
+
 	// Sign in
 
 	$('.js-open-signin').on('click', function() {
